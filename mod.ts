080045f37fd499a99bf5e6402cbf39d8e1633665
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-29 17:57:40 +0700

    Add files via upload

---
+++ mod.ts
@@ -0,0 +1,2 @@
+export * from "./src/telejuh.ts";
+export * from "./src/types.ts";
--- src/telejuh.ts
+++ src/telejuh.ts
@@ -1,17 +1,3 @@
-# Telejuh Deno
-
-A Telegram bot framework built for Deno runtime.
-
-## Installation
-
-```ts
-import {Telejuh} from "jsr:@kartendsy/telejuh";
-```
-
-
-# Docs 
-```ts
-// telejuh.ts
 import type { Events, SendMsgParams, SendPhotoParams, Update} from "./types.ts";
 
 export class Telejuh {
@@ -102,60 +88,3 @@ export class Telejuh {
         }
     }
 }
-
-// types.ts
-/** @module */
-/** A Base Message Parameter function. */
-interface BaseMsgParams {
-    chat_id:number | undefined;
-    parse_mode?:string;
-}
-
-/** A Send Message Patameter function. */
-export interface SendMsgParams extends BaseMsgParams {
-    text:string;
-}
-
-/** A Send Photo Paramenter function. */
-export interface SendPhotoParams extends BaseMsgParams {
-    photo: Uint8Array<ArrayBuffer> | string;
-    caption?: string;
-}
-
-/** A Event on/emit function. */
-export type Events = {
-    message:{
-        chat?:{
-            id?:number,
-            is_bot?:boolean,
-            first_name?:string,
-            username?:string,
-            language_code?:string
-        },
-        text?:string
-    }
-}
-
-/** A Update on polling. */
-export interface Update {
-    update_id:number;
-    message?: {
-        message_id?:number,
-        from?: {
-            id:number,
-            is_bot:boolean,
-            first_name:string,
-            username:string,
-            language_code:string,
-        },
-        chat?:{
-            id:number,
-            first_name:string,
-            username:string,
-            type:string
-        },
-        date?:number,
-        text?:string
-    }
-}
-```

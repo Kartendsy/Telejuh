d7428b0ac778304ac847aefa9f633fa8abe67989
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-29 18:03:43 +0700

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,2 +1,18 @@
 export * from "./src/telejuh.ts";
 export * from "./src/types.ts";
+
+/**
+ * This module Telegram Bot API
+ * 
+ * @example
+ * ```ts
+ * import {Telejuh} from "jsr:@kartendsy/telejuh";
+ * 
+ * const bot = new Telejuh("TOKEN");
+ * 
+ * bot.on("message", (msg) => {
+ *    console.log(msg);
+ * })
+ * 
+ * bot.runPolling();
+ */
